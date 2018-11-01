ea7d4256157b3c18b58ab34532ea24a4fa761e76
Jinsung <jinsung.ha929@gmail.com>
2018-11-01 19:48:08 +0000

    Implemented code for mle plotting but it needs an adjustment

--- cw2/main.py
+++ cw2/main.py
@@ -6,7 +6,42 @@ N = 25
 X = np.reshape(np.linspace(0, 0.9, N), (N, 1))
 Y = np.cos(10*X**2) + 0.1*np.sin(100*X)
 
-def plot_dataset(X, Y):
+def basis_func(x, degree, btype):
+    if btype == 'poly':
+        # Polynomial of degree K
+        return np.array([x**i for i in range(degree+1)])
+    elif btype == 'trigo':
+        # Trigonometric of degree K with unit frequency
+        res = []
+        for i in range(2*degree+1):
+            if i % 2 == 1:
+                v = np.sin(2*np.pi*i*x)
+            else:
+                v = np.cos(2*np.pi*i*x)
+            res.append(v)
+        return res
+    elif btype == 'gauss':
+        # Gaussian with scale l and means mu_j
+        # TODO know where to retrieve the scale l and means mu_j
+        # TODO not sure how to define this
+        return None
+    else:
+        raise ValueError('basis type is invalid')
+
+def build_design_matrix(dataset, basis, degree, btype):
+    return np.asmatrix(np.array(
+            [basis(x, degree, btype) for x in dataset]
+        ).reshape(len(dataset), -1))
+
+def find_optimal_parameters(dm, y):
+    return np.linalg.inv(dm.T*dm) * dm.T * y
+
+def plot_dataset(X, Y, degree, btype, xs, opt_theta):
+
+    # main plotting algorithm
+    plt.plot(xs, basis_func(xs, degree, btype).astype(np.int).T*opt_theta)
+
+    # PLT & UI
     plt.scatter(X, Y)
 
     plt.xlabel('x')
@@ -17,31 +52,11 @@ def plot_dataset(X, Y):
     axes.set_xlim([-0.3, 1.3])
     plt.show()
 
-def basis_func(x, degree, type):
-    if type == 'polynomial':
-        # Polynomial of degree K
-        return np.array([x**i for i in range(degree+1)])
-    elif type == 'trigonometric':
-        # Trigonometric of degree K with unit frequency
-        return 2
-    elif type == 'gaussian':
-        # Gaussian with scale l and means mu_j
-        return 3
-    else:
-        raise ValueError('basis type is invalid')
-
-def build_design_matrix(dataset, basis, degree, type):
-    return np.asmatrix(np.array(
-            [basis_func(x, degree, type) for x in dataset]
-        ).reshape(len(dataset), -1))
-
-def find_optimal_parameters(dm, y):
-    return np.linalg.inv(dm.T*dm) * dm.T * y
-
 if __name__ == '__main__':
-    design_matrix = build_design_matrix(X, basis_func, degree=2, type='polynomial')
+    # Build a design matrix 
+    design_matrix = build_design_matrix(X, basis=basis_func, degree=2, btype='poly')
     opt_theta = find_optimal_parameters(design_matrix, Y)
-    print(opt_theta)
 
-    #plot_dataset(X, Y)
+    x_range = np.linspace(-0.3, 1.3, 500)
+    plot_dataset(X, Y, degree=2, btype='poly', xs=x_range, opt_theta=opt_theta)
 
