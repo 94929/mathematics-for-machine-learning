feabbf59dad8d2d583c7e5da8d670fab8d24360e
Jinsung <jinsung.ha929@gmail.com>
2018-11-14 14:39:59 +0000

    Implemented plot contour function for cw3-b without erroring but the plot needs to be inspected as it is not a correct plot

--- cw3/plot.py
+++ cw3/plot.py
@@ -30,47 +30,63 @@ def gradient_descent(step, step_size):
     # return steps and function values for plotting
     return np.column_stack((alphas, betas)).T
 
-def plot_contour_2d(f, path, maxima):
+def poly_func(order, x):
+    a = np.ones((x.shape[0], order+1))
+    for i in range(1, order+1):
+        a[:, i] = a[:, i-1] * x.ravel()
+    return a
+
+def plot_contour_2d(f, path):
+    
+    # define the basis function
+    Phi = poly_func(1, X)
 
     # preparing the configuration values
-    xmin, xmax, xstep = 0, 3, .1
-    ymin, ymax, ystep = 0, 3, .1
+    # range should begin from non-zero as ln 0 is not defined
+    xmin, xmax, xstep = .1, 3, .1
+    ymin, ymax, ystep = .1, 3, .1
     x, y = np.meshgrid(
                 np.arange(xmin, xmax + xstep, xstep), 
                 np.arange(ymin, ymax + ystep, ystep)
             )
 
-    z = f(x, y, Phi, Y)
-    _maxima = maxima.reshape(-1, 1)
+    # ensure x and y have the same shape
+    assert x.shape == y.shape
+
+    # fill z-values (i.e. the function values)
+    # TODO: check whether the 3 lines of code below works exactly as expected
+    xys = [list(zip(x_i, y_i)) for x_i, y_i in zip(x, y)]
+    flatten_xys = [xys[i][j] for i in range(len(xys)) for j in range(len(xys[0]))]
+    z = np.array([f(x_i, y_i, Phi, Y) for x_i, y_i in flatten_xys]).reshape(x.shape)
+
+    maxima = np.array([path[0, -1], path[1, -1]]).reshape(-1, 1)
 
     # plotting methods
     fig, ax = plt.subplots(figsize=(10, 6))
 
     lvls = np.logspace(-1, 3, 250)
-
     ax.contour(x, y, z, levels=lvls, norm=LogNorm(), cmap=plt.cm.jet)
     ax.quiver(path[0,:-1], path[1,:-1], 
             path[0,1:]-path[0,:-1], path[1,1:]-path[1,:-1], 
             scale_units='xy', angles='xy', scale=1, color='k')
-    ax.plot(*_maxima, 'r*', markersize=18)
+    ax.plot(*maxima, 'r*', markersize=18)
 
     ax.set_xlabel('$x$')
     ax.set_ylabel('$y$')
 
     ax.set_xlim((xmin, xmax))
-    ax.set_ylim((ymin, ymax))
+    ax.set_ylim((xmin, xmax))
 
     plt.show()
 
 if __name__ == '__main__':
     # obtain steps (i.e. path) by running the gradient descent algorithm
-    path = gradient_descent(step=np.array([1., 1.]), step_size=.1)
-    print('Path a:', path[0,:])
-    print('Path b:', path[1,:])
-
-    # set maxima of the given function (should be obtained prior to plot)
-    maxima = np.array([2.21670974, 0.39935876])
+    path = gradient_descent(step=np.array([.75, .75]), step_size=.1)
+    #print('Path a:', path[0,:])
+    #print('Path b:', path[1,:])
+    print('Maxima a:', path[0, -1])
+    print('Maxima b:', path[1, -1])
 
     # plot contour (2d)
-    plot_contour_2d(lml, path, maxima)
+    plot_contour_2d(lml, path)
 
