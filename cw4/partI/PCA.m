c2ea783b7748a432ed0adfef4524c535f4387d09
Jinsung <jinsung.ha929@gmail.com>
2018-11-19 17:48:44 +0000

    Added template for function PCA in PCA.m

--- cw4/partI/PCA.m
+++ cw4/partI/PCA.m
@@ -1,1 +1,13 @@
 % to be completed
+
+% TODO: implement PCA function below so that it can be used in demoPCA.m
+function U_reduc = PCA(train, train_size)
+    % size(train): 340x4096
+    % train_size = 339
+
+    disp('size of fea_Train')
+    disp(train_size)
+
+    %U_reduc = -1
+end
+
