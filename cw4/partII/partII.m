aee1cbe3cd252f32639ae09ae521ba89b9e74898
Jinsung <jinsung.ha929@gmail.com>
2018-11-30 14:43:46 +0000

    Implemented part2

--- cw4/partII/partII.m
+++ cw4/partII/partII.m
@@ -37,8 +37,39 @@ function [] = partII()
 
 end
 
+function k = kernel(x_i, x_j, w)
+    %k = exp(-norm(x_i - x_j,2)^2/w);
+    % non-linear space kernel
+    k = x_i'*x_j/w;
+end
+
 function [a, R] = calcRandCentre(data)
 
-    % to be completed
+    n = size(data, 1);
+    H = 2 * data * data';
+    gl = zeros(n, 1);
+    gu = ones(n , 1);
 
+    % calculate quads
+    quads = quadprog(H, -diag(H), zeros(1, n), 0, ones(1, n), 1, gl, gu);
+
+    % calculate a
+    a = zeros(2, 1);
+    for i = 1:n
+        a = a + data(i, :)' * quads(i);
+    end
+
+    % calculate R
+    R = 0;
+    quad_vec = find(quads>1e-10);
+    n = length(quad_vec);
+    for i = 1:n
+        j = quad_vec(i);
+        R = R + norm(data(j, :)' + a);
+    end
+    R = R/n;
+
+    % disp values
+    disp(a)
+    disp(R)
 end
